d5dbabd5ab535897f62e04e16d7db77771b59b98
Chris-Desktop <Chris@desktop.com>
2026-05-24 13:04:37 -0700

    working on taking file data and processing it into an object.
    Working on .zip, .eml <-- thinking we can use recurrsion to process these, might need to change/create standard processd file object.
    
    llm hook  configured, just need to feed it the proessed data object
    
    idk if it compiles, need to finish doc processing before can troublehoot.

--- Modules/engine_injesting/injest_interface.py
+++ Modules/engine_injesting/injest_interface.py
@@ -9,13 +9,11 @@
 from abc import ABC, abstractmethod
 
 CONST={
-     "PROMPT": "",
-     "MODEL_NAME":"",
-     
-
+     #==== Error codes
      "ERR_CODE":-1,
      "ERR_TXT":"Error occured", 
      
+     #==== Document type supported
      "DOCUMENT_TYPES":{
           ".PDF":[],
           ".DOCX":[],
@@ -25,7 +23,29 @@ CONST={
           ".PPTX":[],
           ".ZIP":[],
           # "":[],
-     }
+     },
+
+     #===== AI instructions
+     "MODEL_NAME":"deepseek-r1:14b",
+     "PROMPT": """
+          read the following document and generate SEO meta tags for it. 
+          Return ONLY JSON OBJECTS with the fileds:
+          
+               -1_Title: under 50 word. 
+               -2_Summary: 100 word summary of the document's content.     
+               -3_Description: up to 300 word description of the content. 
+               -4_Why was the communication sent: use up to 150 words to describe why the communication was sent. 
+               -5_Keywords: list up to 15 relevant key words. 
+               -6_Meta Data: who sent it, when,, when was the doucment created  
+
+               -7_original document title: include the document's original title in the response. 
+               -8_original doucment description: include the document's original description in the response. 
+
+          ONLY JSON OBJECTS will be accepted. 
+               
+          Document:                         
+     """,
+     
 }
 
 
--- Modules/engine_injesting/readme.md
+++ Modules/engine_injesting/readme.md
@@ -1,3 +1,17 @@
 ## scope 
 
-i want to 
+Hard coded values go into the config for the object
+
+create the interace at the end after finishing the object 
+
+injestion engine -- stage 2
+   - will take all documents in the __injest file  
+   - break the fils down into their content  (images, text, tables, headers, footers, ... )
+   - feed one big string object into a local LLM for processing (not CLOUD sensetive work!!! )
+   
+   stage 3
+   - get the output from AI and embed all the response objects into a vector Database (sample files) YT link:https://www.youtube.com/watch?v=god8Pox1laE 
+   
+   stage 4
+   - front end: let the user send a query
+   - backend: catch the query and send it to the database. return obj.
