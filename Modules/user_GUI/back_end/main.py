e900f63b6962fff88d8e8cc579562a07270283b5
Favour Otas <favour2otas@gmail.com>
2026-05-27 21:17:36 -0700

    frontend updates and backend session prep

--- Modules/user_GUI/back_end/main.py
+++ Modules/user_GUI/back_end/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from Modules.user_GUI.back_end.service import load_documents
+from fastapi.responses import JSONResponse
+from Modules.user_GUI.back_end.session_store import create_session, SESSION_STORE
 
 app = FastAPI()
 
@@ -7,6 +9,24 @@ app = FastAPI()
 def get_documents():
     return load_documents()
 
-@app.get("/Documents")
-def get_documents_upperCase():
-    return {"documents": load_documents()}
+@app.get("/session/start")
+def start_session():
+    session_id = create_session()
+    response = JSONResponse(content={"message": "Session started", "session_id": session_id})
+
+    response.set_cookie (
+        key="session_id", 
+        value=session_id, 
+        httponly=True,
+        samesite="lax",
+        secure=False
+    )  # Set to True in production with HTTPS)
+    return response
+
+@app.get("/auth/google/login")
+def google_login():
+    pass
+
+@app.get("/auth/google/callback")
+def google_callback(code: str, state: str):
+    pass
